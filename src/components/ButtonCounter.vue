fbca9c3169422a1a5873d54047f84481281926c2
Alu89 <adel.benamara.89@gmail.com>
2020-04-26 14:05:08 +0200

    add componenet and test props a way to pass data from parent to child component

---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,15 @@
+// Define a new component called button-counter
+<script>
+export default  {
+    name:'button-counter',
+    props:['messageFromParent'],
+    data: function () {
+      return {
+        count: 0
+      }
+    }
+  }
+</script>
+<template>
+  <button v-on:click="count++"> {{messageFromParent}} You clicked me {{ count }} times.</button>
+ </template>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,26 +5,31 @@
       <input id="name" type="text"></input>
       <label for="lastname">Lastname:</label>
       <input id="lastname"/>
-      <button type="submit">Submit</button>
+      <button type="submit">Submit 2</button>
     </form>
-    <input v-model="message" placeholder="edit me">
+    <button-counter v-bind:messageFromParent="message"/>
+    <input v-model="message" placeholder="edit me 2">
     <p>{{message}}</p>
   </div>
 </template>
 
 <script>
 
+import ButtonCounter from './ButtonCounter.vue'
+
 export default {
   name:'Form',
+  components:{ButtonCounter},
   data : function(){
     return{
       greeting:"Hello 2",
       message:''
       };
   },
+
   methods:{
     test: function () {
-      return "Everythin is fine";
+      return "Everythin is fine 2";
     }
   }
 }
